0fd1166585f06d860e2c5fca9da06ee3ac2da312
Md Anas Sabah <mdanassabah@gmail.com>
2022-04-15 17:42:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,92 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const hiragana = [
+    { romanji: "a", hiragana: "あ" },
+    { romanji: "i", hiragana: "い" },
+    { romanji: "u", hiragana: "う" },
+    { romanji: "e", hiragana: "え" },
+    { romanji: "o", hiragana: "お" },
+    { romanji: "ka", hiragana: "か" },
+    { romanji: "ki", hiragana: "き" },
+    { romanji: "ku", hiragana: "く" },
+    { romanji: "ke", hiragana: "け" },
+    { romanji: "ko", hiragana: "こ" },
+    { romanji: "sa", hiragana: "さ" },
+    { romanji: "shi", hiragana: "し" },
+    { romanji: "su", hiragana: "す" },
+    { romanji: "se", hiragana: "せ" },
+    { romanji: "so", hiragana: "そ" },
+    { romanji: "ta", hiragana: "た" },
+    { romanji: "chi", hiragana: "ち" },
+    { romanji: "tsu", hiragana: "つ" },
+    { romanji: "te", hiragana: "て" },
+    { romanji: "to", hiragana: "と" },
+    { romanji: "na", hiragana: "な" },
+    { romanji: "ni", hiragana: "に" },
+    { romanji: "nu", hiragana: "ぬ" },
+    { romanji: "ne", hiragana: "ね" },
+    { romanji: "no", hiragana: "の" },
+    { romanji: "ha", hiragana: "は" },
+    { romanji: "hi", hiragana: "ひ" },
+    { romanji: "fu", hiragana: "ふ" },
+    { romanji: "he", hiragana: "へ" },
+    { romanji: "ho", hiragana: "ほ" },
+    { romanji: "ma", hiragana: "ま" },
+    { romanji: "mi", hiragana: "み" },
+    { romanji: "mu", hiragana: "む" },
+    { romanji: "me", hiragana: "め" },
+    { romanji: "mo", hiragana: "も" },
+    { romanji: "ya", hiragana: "や" },
+    { romanji: "yu", hiragana: "ゆ" },
+    { romanji: "yo", hiragana: "よ" },
+    { romanji: "ra", hiragana: "ら" },
+    { romanji: "ri", hiragana: "り" },
+    { romanji: "ru", hiragana: "る" },
+    { romanji: "re", hiragana: "れ" },
+    { romanji: "ro", hiragana: "ろ" },
+    { romanji: "wa", hiragana: "わ" },
+    { romanji: "wo", hiragana: "を" },
+    { romanji: "n", hiragana: "ん" },
+  ];
+
+  const [input, setInput] = useState("");
+  const [current, setCurrent] = useState(0);
+
+  const [streak, setStreak] = useState(0);
+  const [maxStreak, setMaxStreak] = useState(0);
+
+  const [error, setError] = useState(false);
+
+  const setRandomHirigana = () => {
+    const randomIndex = Math.floor(Math.random() * hiragana.length);
+    setCurrent(randomIndex);
+  };
+
+  const handleChange = (evt) => {
+    setInput(evt.target.value);
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    if (input.toLowerCase() === hiragana[current].romanji) {
+      setStreak(streak + 1);
+      setMaxStreak(Math.max(streak, maxStreak));
+      setError(false);
+      localStorage.setItem("maxStreak", Math.max(streak, maxStreak));
+      localStorage.setItem("streak", streak + 1);
+    } else {
+      setStreak(0);
+      setError(`Wrong! The correct answer is ${hiragana[current].romanji} `);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Hello,World</h1>
+    </div>
+  );
+}
+
+export default App;
